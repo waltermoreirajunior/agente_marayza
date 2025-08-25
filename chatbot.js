2e55e009e3a09868ea06817b1dd38cead0397e70
Walter <waltermoreirasilvajunior@gmail.com>
2025-08-25 12:23:49 -0300

    Create chatbot.js

---
+++ chatbot.js
@@ -0,0 +1,7 @@
+const { useState, useEffect, useRef } = React;
+const { Send, Bot, User, Clock, Calendar, Phone } = lucide;
+
+const ChatBot = () => {
+  // [COLE AQUI TODO O CÓDIGO JAVASCRIPT DO CHATBOT QUE CRIEI ANTERIORMENTE]
+  // O código completo está no artifact anterior
+};
